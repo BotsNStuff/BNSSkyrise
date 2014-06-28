cfbcf50ca79fb5bb2bacb4f350d80e084cb07d4b
harrio34 <hfreni@chadwickschool.org>
2014-06-27 19:05:20 -0700

    Made Code Sexier

--- 1. Autonomous/LCD Animations.c
+++ 1. Autonomous/LCD Animations.c
@@ -1,26 +1,31 @@
-task loadingSpin() {
+task loadingSpin() 
+{
 	const ubyte DELAY = 90;
 	const ubyte NUM_SYMBOLS = 4;
 	const char SYMBOLS[NUM_SYMBOLS] = { '|', '/', '-', '\\' };
 
 	while ( true )
-		for ( ubyte i = 0; i < NUM_SYMBOLS; i++ ) {
+		for ( ubyte i = 0; i < NUM_SYMBOLS; i++ ) 
+        {
 			displayLCDChar( 1, 0, SYMBOLS[i] );
 			wait1Msec( DELAY );
 		}
 }
 
-task loadingBigSpin() {
+task loadingBigSpin() 
+{
 	const ubyte DELAY = 100;
 	const ubyte NUM_SYMBOLS = 4;
 	const char SYMBOLS[NUM_SYMBOLS] = { '/', ' ', '\\', '_' };
 }
 
-task loadingDick() {
+task loadingDick() 
+{
 	const ubyte DELAY = 100;
 	const ubyte SHAFT_LENGTH = 14;
 
-	while ( true ) {
+	while ( true )
+     {
 		clearLCDLine( 1 );
 		for ( ubyte i = 0; i < SHAFT_LENGTH - 2; i++ ) {
 			displayLCDChar( 1, 0, '8' );
@@ -31,22 +36,27 @@ task loadingDick() {
 	}
 }
 
-task winkingFace() {
+task winkingFace() 
+{
 	const ubyte DELAY = 255;
 	const ubyte NUM_SYMBOLS = 2;
 	const char SYMBOLS[NUM_SYMBOLS] = { ':', ';' };
 
 	while ( true )
-		for ( ubyte i = 0; i < NUM_SYMBOLS; i++ ) {
+		for ( ubyte i = 0; i < NUM_SYMBOLS; i++ ) 
+        {
 			displayLCDChar( 1, 2, SYMBOLS[i] );
 			displayLCDChar( 1, 3, 'D' );
 			wait1Msec( DELAY );
 		}
 }
 
-task main() {
+task main() 
+{
 	startTask( loadingBigSpin );
 
-	while ( true ) {
+	while ( true ) 
+    {
+
 	}
 }
