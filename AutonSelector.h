e6bb71dbbde6f9c64e0483ef63dcc6e25c03e005
harrio34 <hfreni@chadwickschool.org>
2014-06-27 18:37:31 -0700

    Primary Commit to The Autonomous Selector

---
+++ AutonSelector.h
@@ -0,0 +1,208 @@
+// BNS Autonomous Selector
+// Made By Harrison Freni of Team 2150D
+// Protected Under the LGPL V3
+
+int count = 0;
+
+// This is how we calculate the battery's life in minutes
+// Change to 2000 for Small Battery, Leave at 3000 for Regular
+int battery = 3000;
+
+// This is how we determine battery life in minutes
+// Change this depending on your motor count
+// For most teams this should be left at the same place
+int motorCount = 10;
+
+// This is the code that actually calculates the battery's life in minutes
+// Do not change this code, or it WILL mess up the readings
+int motorAmperage = motorCount*(amperagePerMotor);
+
+//This code clears LCD lines
+void LCDClear()
+{
+	clearLCDLine(0);
+	clearLCDLine(1);
+}
+
+void waitForRelease()
+{
+	while (nLCDButtons != 0){}
+}
+
+int topLevel()
+{
+	bool selected = false;
+	int menu = -1;
+	LCDClear();
+	while (!selected)
+	{
+		displayLCDCenteredString(0, "<Auton Select>");
+		displayLCDCenteredString(1, "<Batt|Sensors");
+		if (nLCDButtons == 1) 
+		{
+			waitForRelease();
+			menu = 0;
+			picked2 = true;
+		}
+		if (nLCDButtons == 4) 
+		{
+			waitForRelease();
+			menu = 2;
+			picked2 = true;
+		}
+		if (nLCDButtons == 2) 
+		{
+			waitForRelease();
+			menu = 1;
+			picked2 = true;
+		}
+	}
+	return menu;
+}
+
+int sideSelector()
+{
+	bool selected2 = false;
+	LCDClear();
+	while (!selected2)
+	{
+		// This Code Controls How Autonomii are selected
+		displayLCDCenteredString(1, "Select")
+		if (nLCDButtons == 1) 
+		{
+			waitForRelease();
+			count--;
+			if (count < 0) 
+			{
+				count = SIDE_SELECT;
+			}
+		}
+		if (nLCDButtons == 4) 
+		{
+			waitForRelease();
+			count++;
+			if (count > SIDE_SELECT) 
+			{
+				count = 0;
+			}
+		}
+		if (nLCDButtons == 2) 
+		{
+			waitForRelease();
+			menu = 3;
+			selected2 = true;
+		}
+		// This tells our code what to run for certain autonomii
+		switch (count)
+		{
+			//This Code Runs an Autonomous
+			case 0: 
+			{
+				displayLCDCenteredString(0, "");
+				break;
+			}
+
+			//This Code Runs an Autonomous
+			case 1:
+			{
+				displayLCDCenteredString(0, "");
+				break;
+			}
+
+			//This Code Runs an Autonomous
+			case 2:
+			{
+				displayLCDCenteredString(0, "");
+				break;
+			}
+
+			//This Code Runs an Autonomous
+			case 3:
+			{
+				displayLCDCenteredString(0, "");
+				break;
+			}
+		}
+	}
+	return 0;
+}
+
+int codeSelector()
+{
+	bool selected3 = false;
+	LCDClear();
+	while (!selected3)
+	{
+		// This Code Controls How Autonomii are selected
+		displayLCDCenteredString(1, "Select")
+			if (nLCDButtons == 1)
+			{
+			waitForRelease();
+			count--;
+			if (count < 0)
+			{
+				count = STARTING_POS;
+			}
+			}
+		if (nLCDButtons == 4)
+		{
+			waitForRelease();
+			count++;
+			if (count > STARTING_POS)
+			{
+				count = 0;
+			}
+		}
+		if (nLCDButtons == 2)
+		{
+			waitForRelease();
+			picked = true;
+		}
+
+		// This tells our code what to run for certain autonomii
+		switch (count)
+			{
+				//This Code Runs an Autonomous
+			case 0:
+			{
+				displayLCDCenteredString(0, "");
+				break;
+			}
+
+				//This Code Runs an Autonomous
+			case 1:
+			{
+				displayLCDCenteredString(0, "");
+				break;
+			}
+
+				//This Code Runs an Autonomous
+			case 2:
+			{
+				displayLCDCenteredString(0, "");
+				break;
+			}
+
+				//This Code Runs an Autonomous
+			case 3:
+			{
+				displayLCDCenteredString(0, "");
+				break;
+			}
+		}
+	}
+	return 3;
+}
+
+int batteryLife()
+{
+	//Calculate battery life by taking (mAH)/(mA)*0.7
+
+}
+
+int sensorInfo()
+{
+	// Display Sensor info
+	// 2 Pages
+
+}
---
+++ Constants.h
@@ -0,0 +1,17 @@
+// BNS Constant Storage
+// Made By Harrison Freni of Team 2150D
+// Protected Under the LGPL V3
+
+// LCD Auton Selector
+
+// This Value is for Selection of Side 2 More options for
+#define SIDE_SELECT = 4;
+
+// This Value determines how many starting positions there will be
+#define STARTING_POS = 4;
+
+// This Value declares the ammount of pages we have for sensors
+#define SENSOR_PAGES = 2;
+
+// This Valuie declares the ammount of pages we have for battery info
+#define BATTERY_INFO = 2;
